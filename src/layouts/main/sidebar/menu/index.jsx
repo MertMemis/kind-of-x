93741bb37723475ef78a62e8fa9ed79b72ab69e4
MertMemis <mertmemis66@gmail.com>
2023-12-07 22:19:15 +0300

    Update index.jsx

--- src/layouts/main/sidebar/menu/index.jsx
+++ src/layouts/main/sidebar/menu/index.jsx
@@ -1,12 +1,16 @@
 import { NavLink } from "react-router-dom"
 import classNames from "classnames"
+import NewPost from "./post"
+
 
 
 export default function Menu() {
     return (
         <nav className="mt-0.5 mb-1">
-            <NavLink to="/" className="py-1 block group">
+            {mainMenu.map((menu, index) => (
+                <NavLink to={menu.path} className="py-[3px] block group">
                 {({isActive}) =>(
+
                     <div className={classNames("p-3 rounded-full transition-colors inline-flex items-center gap-5 group-hover:bg-[#eff3f41a]", {
                         "font-bold": isActive
                     })}>
