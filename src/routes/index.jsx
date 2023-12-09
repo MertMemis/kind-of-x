f8c2de5daf9dfda6ab4fbb19ca858f8ddcb1a3ee
MertMemis <mertmemis66@gmail.com>
2023-12-09 18:03:36 +0300

    Update index.jsx

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,28 +6,28 @@ import NotFound from "../pages/not-found";
 import MainLayout from "../layouts/main";
 
 const routes = createBrowserRouter([
-    {
-        path: '/',
-        element: <MainLayout />,
-        children: [
-            {
-                index: true,
-                element: <Home />
-            },
-            {
-                path: 'explore',
-                element: <Explore />
-            },
-            {
-                path: 'notifications',
-                element: <Notifications />
-            },
-            {
-                path: '*',
-                element: <NotFound />
-            }
-        ]
-    },
+   {
+      path: '/',
+      element: <MainLayout />,
+      children: [
+         {
+            index: true,
+            element: <Home />
+         },
+         {
+            path: 'explore',
+            element: <Explore />
+         },
+         {
+            path: 'notifications',
+            element: <Notifications />
+         },
+         {
+            path: '*',
+            element: <NotFound />
+         }
+      ]
+   },
     
 ])
 
