69b635c5259c11c3a562af73230aa4db38b0e35e
MertMemis <mertmemis66@gmail.com>
2023-12-09 18:03:41 +0300

    Update index.jsx

--- src/layouts/main/index.jsx
+++ src/layouts/main/index.jsx
@@ -3,21 +3,21 @@ import Sidebar from "./sidebar";
 import RightBar from "./rightbar";
 
 export default function MainLayout() {
-    return (
-        <div className="w-[1265px] mx-auto flex">
+   return (
+      <div className="w-[1265px] mx-auto flex">
 
-            <Sidebar />
+         <Sidebar />
 
-            <main className="flex-1 flex gap-[30px]">
+         <main className="flex-1 flex gap-[30px]">
 
             <main className="flex-1 max-w-[600px] border-x border-[#2f3336]">
-                <Outlet />
+               <Outlet />
             </main>
 
-            <RightBar />
+         <RightBar />
 
-            </main>
+         </main>
 
-        </div>
-    )
+      </div>
+   )
 }
