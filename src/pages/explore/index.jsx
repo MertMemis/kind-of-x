d30decd2480f0519cbfd5d52fffa04ec96c3b658
MertMemis <mertmemis66@gmail.com>
2023-12-09 18:03:31 +0300

    Update index.jsx

--- src/pages/explore/index.jsx
+++ src/pages/explore/index.jsx
@@ -1,7 +1,7 @@
 export default function Explore() {
-    return (
-        <div>
-            explore component!
-        </div>
-    )
+   return (
+      <div>
+         explore component!
+      </div>
+   )
 }
