2f8b7e6773d7f53289a438a435b56e45b48b0805
MertMemis <mertmemis66@gmail.com>
2023-12-09 18:03:25 +0300

    Update index.jsx

--- src/layouts/main/sidebar/menu/post/index.jsx
+++ src/layouts/main/sidebar/menu/post/index.jsx
@@ -2,9 +2,9 @@ import Button from "../../../../../components/button";
 
 
 export default function NewPost() {
-    return(
-        <div className="py-4 w-[90%]">
-            <Button size="large">Post</Button>
-        </div>
-    )
+   return(
+      <div className="py-4 w-[90%]">
+         <Button size="large">Post</Button>
+      </div>
+   )
 }
