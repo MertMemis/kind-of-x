82278a36e5867f4e42ac17bdc408b21ed6042dfa
MertMemis <mertmemis66@gmail.com>
2023-12-10 19:07:40 +0300

    Create index.jsx

---
+++ src/layouts/main/sidebar/account/index.jsx
@@ -0,0 +1,32 @@
+import { Popover } from "@headlessui/react"
+import { useAccount } from "../../../../store/auth/hooks"
+
+
+export default function Account () {
+
+   const account = useAccount()
+
+   return(
+      <div className=" mt-auto ">
+         <Popover>
+            <Popover.Button 
+               className="my-3 p-3 rounded-full hover:bg-[#eff3f41a] transition-colors w-full flex text-left outline-none"
+            >
+               <img src={account.avatar} className=" w-10 h-10 rounded-full" alt="" />
+               <div className=" mx-3 text-[15px]">
+                  <h6 className=" font-bold">{account.fullName}</h6>
+
+                  <div className="text-[#71767b]">
+                     @{account.username}
+                  </div>
+               </div>
+
+               
+            </Popover.Button>
+            <Popover.Panel>
+               hop
+            </Popover.Panel>
+         </Popover>
+      </div>
+   )
+}
