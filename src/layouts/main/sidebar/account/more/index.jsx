549b38fc9c3e51f3cd4824d82225fbe5d02a9ce2
MertMemis <mertmemis66@gmail.com>
2023-12-11 18:46:48 +0300

    Create index.jsx

---
+++ src/layouts/main/sidebar/account/more/index.jsx
@@ -0,0 +1,4 @@
+import classNames from "classnames"
+import { useAccount, useAccounts } from "../../../../../store/auth/hooks"
+import { setCurrentAccount } from "../../../../../store/auth/actions"
+
