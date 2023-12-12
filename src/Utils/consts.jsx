b0a73eab96f7de7e510394f64203eaf8c4ab61ca
MertMemis <mertmemis66@gmail.com>
2023-12-12 20:51:43 +0300

    Update consts.jsx

--- src/Utils/consts.jsx
+++ src/Utils/consts.jsx
@@ -133,6 +133,13 @@ export const mainMenu = [
 
    {
       path: '/messages',
+      title:'Messages',
+      notification: 1,
+      icon: {
+         active: (
+            <svg viewBox="0 0 24 24" width={26.25} height={26.25}>
+               <path
+                  fill="#fff"
 
    {
       path: '/lists',
