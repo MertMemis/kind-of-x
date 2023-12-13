35904b2f97e109b5e8d7cba28c823014621b4298
MertMemis <mertmemis66@gmail.com>
2023-12-13 22:25:15 +0300

    Update index.jsx

--- src/layouts/main/sidebar/menu/post/index.jsx
+++ src/layouts/main/sidebar/menu/post/index.jsx
@@ -3,7 +3,7 @@ import Button from "../../../../../components/button";
 
 export default function NewPost() {
    return(
-      <div className=" pb-[75px] w-full ">
+      <div className=" w-full ">
          <Button size="large">Post</Button>
       </div>
    )
