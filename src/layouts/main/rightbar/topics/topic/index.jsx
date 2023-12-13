56c95e5b81c6a2812def761f9ab886417723735d
MertMemis <mertmemis66@gmail.com>
2023-12-13 22:24:55 +0300

    Update index.jsx

--- src/layouts/main/rightbar/topics/topic/index.jsx
+++ src/layouts/main/rightbar/topics/topic/index.jsx
@@ -2,6 +2,22 @@ import { Link } from "react-router-dom";
 import { numberFormat } from "../../../../../Utils/format";
 import { Popover } from "@headlessui/react";
 
+export default function Topic({ item }) {
+   return (
+      <Link 
+         to="/"
+         className=" py-3 px-4 transition-colors hover:bg-white/[0.03] relative"
+      >
+         <div className=" text-[13px] text-[#71767b] leading-4">{item.title}</div>
+         <div className=" text-[15px] font-bold leading-5 mt-0.5">
+            {item.topic.type === 'tag' && '#'}{item.topic.value}
+         </div>
+         {item?.postCount && (
+            <div className=" text-[13px] mt-1 leading-4 text-[#71767b]">
+               {numberFormat(item.postCount)} posts
+            </div>
+         )}
+
          <Popover className="absolute top-1.5 right-2 focus-within:z-[2]">
             <Popover.Button className=" outline-none w-[34.75px] h-[34.75px] rounded-full flex items-center justify-center hover:bg-[#1d9bf01a] hover:text-[#1d9bf0] transition-colors">
             <svg viewBox="0 0 24 24" width={18.75} height={18.75}>
