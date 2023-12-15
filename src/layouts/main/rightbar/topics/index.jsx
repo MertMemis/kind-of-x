f4835a6763aebe17cc304eb41a8b013a3d1557ed
MertMemis <mertmemis66@gmail.com>
2023-12-15 19:12:29 +0300

    Update index.jsx

--- src/layouts/main/rightbar/topics/index.jsx
+++ src/layouts/main/rightbar/topics/index.jsx
@@ -4,5 +4,11 @@ import SidebarSection from "../../../../components/sidebar-section";
 
 export default function Topics() {
    return (
+      <SidebarSection
+         title="Trends for you"
+         more="/trends"
+      >
+         {topics.map((topic, index) => <Topic item={topic} key={index}/>)}
+      </SidebarSection>
    )
 }
