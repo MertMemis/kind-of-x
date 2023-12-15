47b81469e62ebb348decd4782e798515bdf53206
MertMemis <mertmemis66@gmail.com>
2023-12-15 19:12:41 +0300

    Create index.jsx

---
+++ src/layouts/main/rightbar/who-to-follow/index.jsx
@@ -0,0 +1,5 @@
+import SidebarSection from "../../../../components/sidebar-section";
+import UserCard from "../../../../components/user-card";
+import { whoToFlwUsers } from "../../../../mock";
+import {useAccount} from "../../../../store/auth/hooks";
+
