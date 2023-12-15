1fc6a80f695fd365d0efe0bda30ee22229b5ff0c
MertMemis <mertmemis66@gmail.com>
2023-12-15 19:12:49 +0300

    Update index.jsx

--- src/layouts/main/rightbar/who-to-follow/index.jsx
+++ src/layouts/main/rightbar/who-to-follow/index.jsx
@@ -7,3 +7,12 @@ export default function WhoToFollow() {
 
    const account = useAccount()
 
+   return(
+      <SidebarSection
+         title="Who to follow"
+         more={`/connect_people?user_id=${account.id}`}
+      >
+         {whoToFlwUsers.map(user => <UserCard user={user} key={user.id}/>)}
+      </SidebarSection>
+   )
+}
