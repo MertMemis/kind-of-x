05b7e74b4e639c3d4f8b2a37929570b62c4a6be7
MertMemis <mertmemis66@gmail.com>
2023-12-15 19:13:03 +0300

    Update index.js

--- src/mock/index.js
+++ src/mock/index.js
@@ -5,3 +5,9 @@ export const whoToFlwUsers = [
       fullName: 'Futures Elden Lord',
       avatar: 'https://i.redd.it/x6kvw3djx1571.jpg'
    },
+
+   {
+      id: 4,
+      username: 'PatchesTheUntethered',
+      fullName: 'Patches',
+      avatar: 'https://assetsio.reedpopcdn.com/elden-ring-patches-3-(1).png?width=1200&height=1200&fit=crop&quality=100&format=png&enable=upscale&auto=webp'
