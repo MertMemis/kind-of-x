82a7dda8b777e5d65636598e68ceef59d8313b6d
MertMemis <mertmemis66@gmail.com>
2023-12-22 22:30:46 +0300

    Update index.jsx

--- src/layouts/main/index.jsx
+++ src/layouts/main/index.jsx
@@ -3,12 +3,34 @@ import Sidebar from "./sidebar";
 import RightBar from "./rightbar";
 import { useModal } from "../../store/modal/hooks";
 import Modal from "../../modals";
+import { useEffect } from "react";
+import { useDisplay } from "../../store/display/hooks";
 
 
 
 export default function MainLayout() {
 
    const modal = useModal()
+   const display = useDisplay()
+
+   useEffect(() => {
+      
+      document.documentElement.style.setProperty('--background-primary', display.backgroundColor.primary)
+      document.documentElement.style.setProperty('--background-secondary', display.backgroundColor.secondary)
+      document.documentElement.style.setProperty('--background-third', display.backgroundColor.third)
+      
+      document.documentElement.style.setProperty('--color-primary', display.color.primary)
+      document.documentElement.style.setProperty('--color-secondary', display.color.secondary)
+      document.documentElement.style.setProperty('--color-base', display.color.base)
+      document.documentElement.style.setProperty('--color-base-secondary', display.color.baseSecondary)
+      
+      document.documentElement.style.setProperty('--box-shadow', display.boxShadow)
+      
+      document.documentElement.style.setProperty('--font-size', display.fontSize + 'px')
+
+      console.log('display', display)
+      
+   }, [display])
 
    return (
       <div className="w-[1265px] mx-auto flex">
@@ -18,7 +40,7 @@ export default function MainLayout() {
 
          <main className="flex-1 flex gap-[30px]">
 
-            <main className="flex-1 max-w-[600px] border-x border-[#2f3336]">
+            <main className="flex-1 max-w-[600px] border-x border-[color:var(--background-third)]">
                <Outlet />
             </main>
 
