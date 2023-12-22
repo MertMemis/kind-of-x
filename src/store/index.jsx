541bb06c0c59ce424048b77fbffd01e52418d4ae
MertMemis <mertmemis66@gmail.com>
2023-12-22 22:31:17 +0300

    Update index.jsx

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import auth from "./auth"; 
 import modal from "./modal"; 
+import display from "./display"; 
 
 
 const store = configureStore({
     reducer: {
         auth,
         modal,
+        display,
     }
 })
 
