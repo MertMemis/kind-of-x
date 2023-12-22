f2d6adff9a7186185f7147c2f348cb22f95f5df3
MertMemis <mertmemis66@gmail.com>
2023-12-22 22:30:52 +0300

    Update index.jsx

--- src/layouts/main/rightbar/search/index.jsx
+++ src/layouts/main/rightbar/search/index.jsx
@@ -4,7 +4,7 @@ import { useClickAway } from "react-use"
 export default function Search () {
 
    const [query, setQuery] = useState('')
-   const [changeColor, setChangeColor] = useState('#71767b')
+   const [changeColor, setChangeColor] = useState('color:var(--color-base-secondary)')
    const [focus, setFocus] = useState (false)
    
    const ref = useRef()
@@ -15,20 +15,20 @@ export default function Search () {
 
    function onfcsFunction() {
       setFocus(true);
-      setChangeColor('#1d9bf0');
+      setChangeColor('color:var(--color-primary)');
    }
 
    function onblrFunction() {
-      setChangeColor('#71767b');
+      setChangeColor('color:var(--color-base-secondary)');
    }
 
    return (
       <div 
          ref={ref}
-         className=" min-h-[32px] h-[53px] mb-3 flex items-center sticky top-0 z-[10] bg-black">
-         <label className=" relative h-[43px] rounded-full bg-[#202327] block w-full group border-2 border-transparent focus-within:border-[#1d9bf0]">
+         className=" min-h-[32px] h-[53px] mb-3 flex items-center sticky top-0 z-[10] bg-[color:var(--background-primary)]">
+         <label className=" relative h-[43px] rounded-full bg-[color:var(--background-third)] block w-full group border-2 border-transparent focus-within:border-[color:var(--color-primary)]">
             <div className="w-[56px] h-full flex items-center justify-center absolute top-0 left-0 pointer-events-none">
-               <svg viewBox="0 0 24 24" className={`min-w-[32px] text-[${ changeColor }]`} width={18.75} height={18.75}>
+               <svg viewBox="0 0 24 24" className={`min-w-[32px] text-[${ changeColor }] `} width={18.75} height={18.75}>
                   <path 
                      fill="currentColor"
                      d="M 10.25 3.75 c -3.59 0 -6.5 2.91 -6.5 6.5 s 2.91 6.5 6.5 6.5 c 1.795 0 3.419 -0.726 4.596 -1.904 c 1.178 -1.177 1.904 -2.801 1.904 -4.596 c 0 -3.59 -2.91 -6.5 -6.5 -6.5 Z m -8.5 6.5 c 0 -4.694 3.806 -8.5 8.5 -8.5 s 8.5 3.806 8.5 8.5 c 0 1.986 -0.682 3.815 -1.824 5.262 l 4.781 4.781 l -1.414 1.414 l -4.781 -4.781 c -1.447 1.142 -3.276 1.824 -5.262 1.824 c -4.694 0 -8.5 -3.806 -8.5 -8.5 Z" 
@@ -37,7 +37,7 @@ export default function Search () {
             </div>
             <input 
                type ="text" 
-               className =" w-full h-full bg-transparent placeholder-[#71767b] rounded-full outline-none pl-[56px] text-[15px] focus-within:bg-black" 
+               className =" w-full h-full bg-transparent rounded-full outline-none pl-[56px] text-[15px] focus-within:bg-[color:var(--background-primary)] group-focus-within:text[color:var(--color-primary)]" 
                placeholder="Search"
                value = {query}
                onChange={e => setQuery(e.target.value)}
@@ -48,7 +48,7 @@ export default function Search () {
                <button 
                   type="button"
                   onClick={() => setQuery('')}
-                  className=" w-[22px] h-[22px] rounded-full bg-[#1d9bf0] flex items-center justify-center text-black min-w-[22px] right-3 absolute top-1/2 -translate-y-1/2 "
+                  className=" w-[22px] h-[22px] rounded-full bg-[color:var(--color-primary)] flex items-center justify-center text-color:var(--background-primary) min-w-[22px] right-3 absolute top-1/2 -translate-y-1/2 "
                >
                   <svg viewBox="0 0 15 15" width={10} height={10}>
                      <path 
@@ -61,8 +61,8 @@ export default function Search () {
          </label>
          
          {focus && (
-               <div className=" absolute top-full w-[350px] -translate-y-1 bg-black shadow-box text-center min-h-[100px] max-h-[calc(80vh -53px)] rounded-lg">
-                  <p className=" p-3 pt-5 text-[#71767b]">
+               <div className=" absolute top-full w-[350px] -translate-y-1 bg-[color:var(--background-primary)] shadow-box text-center min-h-[100px] max-h-[calc(80vh -53px)] rounded-lg">
+                  <p className=" p-3 pt-5 text-[color:var(--color-base-secondary)]">
                      Try searching for people, lists, or keywords
                   </p>
                </div>
