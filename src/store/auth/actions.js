22c0bbe980c665b43d453eb517d4e4c2549d236b
MertMemis <mertmemis66@gmail.com>
2023-12-24 01:03:29 +0300

    Update actions.js

--- src/store/auth/actions.js
+++ src/store/auth/actions.js
@@ -1,4 +1,4 @@
 import { _setCurrentAccount } from ".";
-import store from "../../store/auth";
+import store from "../../store/auth/index";
 
-export const setCurrentAccount = data => store.dispatch (_setCurrentAccount(data))
+export const setCurrentAccount = data => store.dispatch(_setCurrentAccount(data))
