60191572e592124a886260b2c33df4e7b42272fe
MertMemis <mertmemis66@gmail.com>
2023-12-24 01:03:22 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import './assets/css/tailwind.css'
 import { Provider } from 'react-redux'
 import store from './store'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 
 <Provider store={store}>
