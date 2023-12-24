eaf0d55644be743bfd420f710d1eabc16915068a
MertMemis <mertmemis66@gmail.com>
2023-12-24 19:10:18 +0300

    Update index.jsx

--- src/layouts/main/sidebar/account/index.jsx
+++ src/layouts/main/sidebar/account/index.jsx
@@ -13,16 +13,16 @@ export default function Account () {
             <Popover.Button 
                className=" p-3 my-3 rounded-full hover:bg-[color:var(--background-third)] transition-colors w-[260px] flex text-left items-center outline-none"
             >
-               <img src={account.avatar} className=" w-10 h-10 rounded-full" alt="" />
-               <div className=" mx-3 text-[15px]">
-                  <h6 className=" font-bold">{account.fullName}</h6>
+               <img src={account.avatar} className=" w-10 h-10 rounded-full flex-shrink-0" alt="" />
+               <div className=" mx-3">
+                  <h6 className=" font-bold leading-[1.25rem]">{account.fullName}</h6>
 
                   <div className="text-[color:var(--color-base-secondary)]">
                      @{account.username}
                   </div>
                </div>
 
-               <svg viewBox="0 0 24 24" className="  ml-auto" width={18.75}>
+               <svg viewBox="0 0 24 24" className="  ml-auto h-[1.172rem]">
                   <path
                      fill="currentColor"
                      d="M 3 12 c 0 -1.1 0.9 -2 2 -2 s 2 0.9 2 2 s -0.9 2 -2 2 s -2 -0.9 -2 -2 Z m 9 2 c 1.1 0 2 -0.9 2 -2 s -0.9 -2 -2 -2 s -2 0.9 -2 2 s 0.9 2 2 2 Z m 7 0 c 1.1 0 2 -0.9 2 -2 s -0.9 -2 -2 -2 s -2 0.9 -2 2 s 0.9 2 2 2 Z"
