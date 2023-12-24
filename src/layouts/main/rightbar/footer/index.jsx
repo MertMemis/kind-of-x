3a43ca35a534c320895d831238c865404786f011
MertMemis <mertmemis66@gmail.com>
2023-12-24 19:10:07 +0300

    Update index.jsx

--- src/layouts/main/rightbar/footer/index.jsx
+++ src/layouts/main/rightbar/footer/index.jsx
@@ -4,16 +4,17 @@ import { Link } from "react-router-dom";
 export default function Footer() {
    return(
       <footer className=" mb-4 px-4 flex flex-wrap gap-2">
-         <Link className="text-[color:var(--color-base-secondary)] leading-4 text-[14px] hover:underline" to="/">Terms of Service</Link>
-         <Link className="text-[color:var(--color-base-secondary)] leading-4 text-[14px] hover:underline" to="/">Privacy Policy</Link>
-         <Link className="text-[color:var(--color-base-secondary)] leading-4 text-[14px] hover:underline" to="/">Cookie Policy</Link>
-         <Link className="text-[color:var(--color-base-secondary)] leading-4 text-[14px] hover:underline" to="/">Imprint</Link>
-         <Link className="text-[color:var(--color-base-secondary)] leading-4 text-[14px] hover:underline" to="/">Accessibility</Link>
-         <Link className="text-[color:var(--color-base-secondary)] leading-4 text-[14px] hover:underline" to="/">Ads info</Link>
-         <div className=" w-full flex gap-2">
+         <Link to="/" className="text-[color:var(--color-base-secondary)] leading-4 hover:underline">Terms of Service</Link>
+         <Link to="/" className="text-[color:var(--color-base-secondary)] leading-4 hover:underline">Privacy Policy</Link>
+         <Link to="/" className="text-[color:var(--color-base-secondary)] leading-4 hover:underline">Cookie Policy</Link>
+         <Link to="/" className="text-[color:var(--color-base-secondary)] leading-4 hover:underline">Imprint</Link>
+         <Link to="/" className="text-[color:var(--color-base-secondary)] leading-4 hover:underline">Accessibility</Link>
+         <Link to="/" className="text-[color:var(--color-base-secondary)] leading-4 hover:underline">Ads info</Link>
+
+         <div className="inline-flex gap-2 flex-wrap">
             <Popover className="relative leading-4">
                <Popover.Button 
-                  className="text-[color:var(--color-base-secondary)] text-[14px] hover:underline outline-none inline-flex items-center">
+                  className="text-[color:var(--color-base-secondary)] hover:underline outline-none inline-flex items-center">
                   More
                   <svg viewBox="0 0 24 24" className="h-[1em] px-0.5">
                         <path 
@@ -22,29 +23,29 @@ export default function Footer() {
                         />
                   </svg>
                </Popover.Button>
-               <Popover.Panel className="w-[150px] max-w-[384px] absolute rounded-xl bottom-0 right-0 bg-[color:var(--background-primary)] shadow-box grid overflow-hidden">
-                  <Link to="/" className=" py-3 px-4 text-[15px] leading-5 transition-colors hover:bg-[color:var(--background-secondary)]">
+               <Popover.Panel className="w-[9.375rem] max-w-[24rem] absolute rounded-xl bottom-0 right-0 bg-[color:var(--background-primary)] shadow-box grid overflow-hidden">
+                  <Link to="/" className=" py-3 px-4 leading-5 transition-colors hover:bg-[color:var(--background-secondary)]">
                      About
                   </Link>
 
-                  <Link to="/" className=" py-3 px-4 text-[15px] leading-5 transition-colors hover:bg-[color:var(--background-secondary)]">
+                  <Link to="/" className=" py-3 px-4 leading-5 transition-colors hover:bg-[color:var(--background-secondary)]">
                      Download the X app
                   </Link>
 
-                  <Link to="/" className=" py-3 px-4 text-[15px] leading-5 transition-colors hover:bg-[color:var(--background-secondary)]">
+                  <Link to="/" className=" py-3 px-4 leading-5 transition-colors hover:bg-[color:var(--background-secondary)]">
                      Status
                   </Link>
 
-                  <Link to="/" className=" py-3 px-4 text-[15px] leading-5 transition-colors hover:bg-[color:var(--background-secondary)]">
+                  <Link to="/" className=" py-3 px-4 leading-5 transition-colors hover:bg-[color:var(--background-secondary)]">
                      X for Business
                   </Link>
 
-                  <Link to="/" className=" py-3 px-4 text-[15px] leading-5 transition-colors hover:bg-[color:var(--background-secondary)]">
+                  <Link to="/" className=" py-3 px-4 leading-5 transition-colors hover:bg-[color:var(--background-secondary)]">
                      Developers
                   </Link>
                </Popover.Panel>
             </Popover>
-            <p className="text-[color:var(--color-base-secondary)] leading-4 text-[14px]"> 
+            <p className="text-[color:var(--color-base-secondary)] leading-4"> 
                &copy; 2024 Mert Memiş
             </p>
          </div>
