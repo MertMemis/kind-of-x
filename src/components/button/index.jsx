6b5331642b43fae94ff9570c290e33400484bd8d
MertMemis <mertmemis66@gmail.com>
2023-12-24 19:10:03 +0300

    Update index.jsx

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -7,7 +7,7 @@ export default function Button({ as, size, variant, className, children, ...prop
       className: classNames("rounded-full flex items-center justify-center font-bold transition-all", {
          "px-4 h-8 text-sm": size === 'small',
          "px-4 h-9": size === 'normal',
-         "px-4 text-[17px] h-[52px] w-[90%]": size === 'large',
+         "px-4 text-[1.063rem] h-[3.25rem] w-[90%]": size === 'large',
          "bg-[color:var(--color-primary)] hover:opacity-85 text-white": variant === 'primary',
          "bg-[color:var(--color-base)] text-[color:var(--background-primary)] hover:bg-[#d7dbdc]": variant === 'white',
          "border border-[#b4b4b4] hover:border-[#67070f] hover:bg-[#f4212e1a] hover:text-[#f4212e]": variant === 'white-outline',
