a8b2301e7e4d00850dd400cf645c9ec5d7a9fbb5
MertMemis <mertmemis66@gmail.com>
2023-12-25 23:56:55 +0300

    Update index.jsx

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,20 @@
 import MainLayout from "../../layouts/main";
+import StickyHeader from "../../components/sticky-header";
+import Tab from "../../components/tab";
 
 
 export default function Home() {
    return (
-      <div>
-         home component!
-      </div>
+      <>
+			<Tab activeTab="followings">
+				<StickyHeader title="Anasayfa">
+					<Tab.Items>
+						<Tab.Item id="for-you">
+							Sana özel
+						</Tab.Item>
+						<Tab.Item id="followings">
+							Takip edilenler
+						</Tab.Item>
         
    )
 }
