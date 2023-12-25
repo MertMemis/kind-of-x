c6b35ba155a2b45192e4f1899a08fea30dacbade
MertMemis <mertmemis66@gmail.com>
2023-12-25 23:56:46 +0300

    Create post.js

---
+++ src/mock/post.js
@@ -0,0 +1,38 @@
+export const posts = [
+	{
+		id: '1708632465282150796',
+		content: `Toplu günaydın, ben her sabah spora devam ama her gün post atmıyorum artık, max 4 günde bir atarım çünkü en fazla 4 foto paylaşılıyormuş 🤣`,
+		photos: [
+			'https://pbs.twimg.com/media/F7VBIvaW0AIZyiX?format=jpg&name=large',
+			'https://pbs.twimg.com/media/F7VBIvZXEAAIpEM?format=jpg&name=large',
+			'https://pbs.twimg.com/media/F7VBIvaW0AAyXEr?format=jpg&name=large',
+			'https://pbs.twimg.com/media/F7VBIvaWAAAWUsx?format=jpg&name=large',
+		],
+		account: {
+			avatar: 'https://pbs.twimg.com/profile_images/1665642354009677824/LNZo7V8x_400x400.jpg',
+			username: 'tayfunerbilen',
+			fullName: 'Tayfun Erbilen'
+		},
+		stats: {
+			comments: 43535,
+			repost: 3434,
+			like: 96946,
+			view: 903453
+		}
+	},
+	{
+		id: '1708632465282150796',
+		content: `Imagination Land!`,
+		photos: [
+			'https://pbs.twimg.com/media/F7ZIK8KWgAAP8pH?format=jpg&name=900x900'
+		],
+		account: {
+			avatar: 'https://pbs.twimg.com/profile_images/1683325380441128960/yRsRRjGO_400x400.jpg',
+			username: 'elonmusk',
+			fullName: 'Elon Musk',
+			verified: true
+		},
+		stats: {
+			comments: 6573535,
+			repost: 343434,
+			like: 366946,
