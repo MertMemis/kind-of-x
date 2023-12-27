7f877e77e81e805f0f1c20f984b4d11e57f0ed77
MertMemis <mertmemis66@gmail.com>
2023-12-27 17:11:09 +0300

    Create index.jsx

---
+++ src/pages/home/for-you/index.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+import Post from "../../../components/post"
+import {posts as postData} from "../../../mock/posts"
+import { WVList } from "virtua"
+
+
+
+export default function ForYou() {
+
+   const [posts, setPosts] = useState ([...postData, ...postData, ...postData, ...postData, ...postData, ...postData])
+
+
+   return(
+      <WVList
+			onRangeChange={(start, end) => {
+				console.log('end', end + 1)
+				if (end + 1 === posts.length) {
