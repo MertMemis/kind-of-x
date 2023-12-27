2276c45f8b0a64e18100eee0888dd93f546c20c4
MertMemis <mertmemis66@gmail.com>
2023-12-27 17:10:16 +0300

    Update index.jsx

--- src/layouts/main/sidebar/index.jsx
+++ src/layouts/main/sidebar/index.jsx
@@ -5,7 +5,7 @@ import Menu from "./menu"
 
 export default function Sidebar() {
    return(
-      <aside className="w-[268px] min-h-screen max-h-screen flex flex-col sticky top-0">
+      <aside className="w-[268px] min-h-screen max-h-screen flex flex-col z-[2] sticky top-0">
          <Logo />
          <Menu />
          <Account />
